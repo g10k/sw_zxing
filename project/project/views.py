6983f4bb1053df276adcc95d75fdea0cd59f57c7
g10k <g10k.info@gmail.com>
2016-09-14 12:58:18 +0300

    убрал принты

--- project/project/views.py
+++ project/project/views.py
@@ -13,14 +13,8 @@ def upload(request):
         - name: filename
           type: file
     '''
-    print 'post', request.POST
-    print 'files', request.FILES
-    print 'files:', request.FILES['filename']
     file_obj = request.FILES['filename']
-    print 'file_obj: ', file_obj
     reader = zxing.BarCodeReader(settings.ZXING_PATH)
-    print file_obj.temporary_file_path()
     barcode = reader.decode(file_obj.temporary_file_path(), try_harder=True, qr_only=False)
     raw_text = barcode.raw
-    print "raw_text: ", raw_text
     return JsonResponse({'text': raw_text})
