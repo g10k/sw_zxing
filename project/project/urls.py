191d73b3b2b7bca481443e8ee16b07b5bd9700b8
g10k <g10k.info@gmail.com>
2016-09-23 17:51:59 +0300

    добавил в nginx параметр чтобы работал django-swagger

--- project/project/urls.py
+++ project/project/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+from django.views.generic import RedirectView
 import views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='/docs')),
     url(r'^upload/', views.upload),
     url(r'^docs/', include('rest_framework_swagger.urls', namespace='docs')),
 ]
